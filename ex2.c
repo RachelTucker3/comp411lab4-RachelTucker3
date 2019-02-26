11a56d37c7f667cda0bf1f9bdd4090be5886b1b6
Rachel Tucker <rach1234@live.unc.edu>
2019-02-26 02:50:22 +0000

    replace this with a useful message

--- ex2.c
+++ ex2.c
@@ -1,3 +1,4 @@
+/* Example: bubble sort strings in array */
 
 #include <stdio.h>  /* Need for standard I/O functions */
 #include <string.h> /* Need for strlen() */
@@ -7,79 +8,6 @@
 #define LEN 1000  /* max length of each string */
 
 
-
-int my_compare_strings(char string1[], char string2[]) {
-
-  for(int x = 0; x < LEN; x++) {
-
-    if(string1[x] != string2[x]) {
-
-      if(string1[x] < string2[x]) {
-
-        return -1;
-
-      } if(string1[x] > string2[x]) {
-
-          return 1;
-
-      }
-
-    }else {
-
-      return 0;
-
-    }
-
-  }
-
-
-
-/* Write code here to compare string1 and string2, character by character.
-     WITHOUT USING ANY C STRING LIBRARY FUNCTIONS.
-
-     The max length of the strings is LEN, though they may be shorter so look
-     for the string terminators.
-
-     This function should return:
-      -1 if string1 comes before string2 in alphabetical order
-       0 if string1 is the same as string2
-      +1 if string1 comes after string2 in alphabetical order
-
-      Here, alphabetical order is defined simply by the numerical value
-      of the characters in the string, from left to right.  See the writeup
-      for more details.
-  */
-}
-
-
-void my_swap_strings(char string1[], char string2[]) {
-  char temp;    // char variable used in swapping one character at a time
-
-  /* Write code here to swap the contents of string1 and string2, one
-     character at a time, WITHOUT USING ANY C STRING LIBRARY FUNCTIONS.
-
-     The max length of the strings is LEN, though they may be shorter, so look
-     for the string terminators.
-  */
-
-  for(int i = 0; i < LEN; i++) {
-
-    char str1 = string1[i];
-    char str2 = string2[i];
-
-  //  if(str1 == NULL char && str2 == NULL char) {
-
-    //  return;
-
-    //}
-
-    temp = string1[i];
-    string1[i] = string2[i];
-    string2[i] = temp;
-
-  }
-
-}
 
 
 int main()
@@ -112,8 +40,32 @@ int main()
   }
 
   /* Write a for loop here to print all the strings. */
+  /* Bubble sort */
+  /* Write code here to bubble sort the strings in ascending alphabetical order
 
+     Use the function my_compare_strings() to compare two strings.  If they are out of order,
+     use the function my_swap_strings() to swap their contents.
 
+  */
+
+	for( int i = 0; i < NUM; i++) {
+
+		for(int j = 0; j < NUM - 1; j++) {
+
+			if(strcmp(Strings[j], Strings[j +1]) > 0) {
+				char tempstring[LEN];
+
+				strcpy(tempstring, Strings[j]);
+				strcpy(Strings[j], Strings[j+1]);
+				strcpy(Strings[j+1], tempstring);
+			}
+
+		}
+
+	}
+  
+  /* Output sorted list */
+  
   puts("\nIn alphabetical order, the strings are:");
   /* Write a for loop here to print all the strings. Feel free to use puts/printf
      etc. for printing each string.
@@ -121,30 +73,9 @@ int main()
 
   for(int i = 0; i < NUM; i++) {
 
-    if(my_compare_strings(Strings[i], Strings[i+1]) == 1 || my_compare_strings(Strings[i], Strings[i+1]) == 0) {
-
-      i = i;
-
-    } else {
-
-  for(int i = 0; i < NUM; i++) {
-
-    if(my_compare_strings(Strings[i], Strings[i+1]) == -1) {
-
-      my_swap_strings(Strings[i], Strings[i+1]);
-
-    }
-
-    }
-
-  }
-
-  }
-
-  for(int i = 0; i < NUM; i++) {
-
     printf("%s\n", Strings[i]);
 
   }
+
 
 }
